dbff3b849f203c7f3e1ef3a712f40305bbd70379
Doug Nottingham <Lownslow200sx@gmail.com>
2015-01-19 20:37:15 -0500

    Add WiFi scan interval to props

--- system_prop.mk
+++ system_prop.mk
@@ -76,3 +76,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.qualcomm.sns.daemon=e \
     debug.qualcomm.sns.hal=e \
     debug.qualcomm.sns.libsensor1=e
+
+# WiFi
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.supplicant_scan_interval=120
